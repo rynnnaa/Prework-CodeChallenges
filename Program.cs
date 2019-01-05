18275180225b1033d3c531f0ecdb07dd612c5ed9
Ryna <pinktulips@Hotmail.com>
2019-01-04 17:35:17 -0800

    challenge1done

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace preworkChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrOfNum = new int[] { 3, 2, 5, 6, 8, 1 };
+            Console.WriteLine("Select from the following numbers to get an output 3, 2, 5, 6, 8, 1");
+            int response = Int32.Parse(Console.ReadLine());
+            userOutput(arrOfNum, response);
+            Console.ReadLine();
+        }
+
+        public static void userOutput(int[] arrOfNum, int response)
+        {
+
+            int output = 0;
+            for (int i = 0; i < arrOfNum.Length; i++)
+            {
+                if (response == arrOfNum[i])
+                    output += arrOfNum[i];
+            }
+            Console.WriteLine($"your output is {output}");
+        }
+    }
+}
